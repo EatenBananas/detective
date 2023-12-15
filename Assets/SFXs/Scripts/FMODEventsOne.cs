5dc213187cfbf0ef07e51fe0f189c43b93084576
jckobSM <jkapler@edu.cdv.pl>
2023-12-15 16:00:43 +0100

    AudioManager, 1ShotSound
    easy way to play one shot sounds

---
+++ Assets/SFXs/Scripts/FMODEventsOne.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class FMODEventsOne : MonoBehaviour
+{
+    [field: Header("Menu")]
+    [field: SerializeField] public EventReference buttonClick { get; private set; }
+
+    public static FMODEventsOne instance { get; private set; }
+
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogError("Found more than one FMOD Events One instance in the scene");
+        }
+        instance = this;
+    }
+}
---
+++ Assets/SFXs/Scripts/OneShotSound.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class OneShotSound : MonoBehaviour
+{
+    public static OneShotSound instace { get; private set; }
+
+
+    private void Awake()
+    {
+        if(instace != null)
+        {
+            Debug.LogError("Found more than one 1ShotSound in the scene");
+        }
+        instace = this;
+    }
+    public void PlayOneShot(EventReference sound, Vector3 worldPos)
+    {
+        RuntimeManager.PlayOneShot(sound, worldPos);
+    }
+
+}
