07601863e6831c0aac9d34e780896dc926005d7e
Janek <jasiojfd@gmail.com>
2024-02-22 18:48:21 +0100

    fix camera not following player after teleport

--- Assets/Scripts/PlayerSystem/Player.cs
+++ Assets/Scripts/PlayerSystem/Player.cs
@@ -1,5 +1,7 @@
+using CameraSystem;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using Zenject;
 
 namespace PlayerSystem
 {
@@ -9,6 +11,8 @@ namespace PlayerSystem
         [field: SerializeField] public PlayerInput Input { get; private set; }
         [field: SerializeField] public HumanoidMovement Movement { get; private set; }
         
+        [Inject] CameraController _cameraController;
+        
         [Button]
         public void Teleport(Vector3 position, bool resetPlayerDestination = true)
         {
@@ -17,6 +21,12 @@ namespace PlayerSystem
             
             //transform.position = position;
             Movement.Agent.Warp(position);
+
+            _cameraController.CameraMode = CameraMode.FollowTarget;
+            
+            var player = transform;
+            _cameraController.FollowTarget = player;
+            _cameraController.LookAtTarget = player;
         }
 
         [Button]
